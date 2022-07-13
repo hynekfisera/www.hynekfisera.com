28dd9bb5ca6733684d7efd233733b8579101d24c
Hynek Fišera <hynek.fisera@seznam.cz>
2022-07-13 20:12:05 +0200

    Update headline & ejs module bug fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,8 +6,10 @@ const Home: NextPage = () => {
   return (
     <>
       <main className="max-w-screen-2xl mx-auto px-4 2xl:px-0">
-        <h1 className="mt-16 text-5xl max-w-[90%] font-bold leading-snug">Full-stack web developer and YouTube content creator also interested in UI/UX design and digital art</h1>
-        <h2 className="mt-4 text-lg font-medium">
+        <h1 className="mt-16 text-5xl max-w-[90%] font-bold leading-snug">
+          Full-stack web <span className="text-indigo-600">developer</span> and YouTube <span className="text-indigo-600">content creator</span> also interested in UI/UX design and digital art
+        </h1>
+        <h2 className="mt-4 text-lg font-medium text-indigo-600">
           Check out my projects <FontAwesomeIcon icon={faChevronDown} className="ml-2" />
         </h2>
       </main>
