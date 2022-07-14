2d21ea42a40273283473d6932bc1ac0c71177d7e
Hynek Fišera <hynek.fisera@seznam.cz>
2022-07-14 09:57:35 +0200

    Close navbar after clicking on a link

--- components/Header.tsx
+++ components/Header.tsx
@@ -28,13 +28,17 @@ export default function Header() {
   return (
     <header className="max-w-screen-2xl mx-auto p-8 flex justify-between items-center">
       <Link href="/">
-        <a className="select-none text-lg font-semibold uppercase tracking-wider">Hynek Fišera</a>
+        <a onClick={() => setVisible(false)} className="select-none text-lg font-semibold uppercase tracking-wider">
+          Hynek Fišera
+        </a>
       </Link>
       <FontAwesomeIcon icon={faBars} className={`text-xl p-1 text-slate-700 sm:!hidden cursor-pointer`} onClick={() => setVisible((v) => !v)} />
       <div className={`${visible ? "flex" : "hidden"} absolute sm:static bg-gray-50 sm:bg-transparent top-0 left-0 right-0 mt-20 sm:mt-0 pl-8 pb-8 sm:p-0 border-b sm:border-none sm:!flex flex-col sm:flex-row gap-2 sm:gap-6`}>
         {links.map((link) => (
           <Link href={link.href} key={link.href}>
-            <a className="select-none uppercase tracking-wider hover:opacity-75">{link.text}</a>
+            <a onClick={() => setVisible(false)} className="select-none uppercase tracking-wider hover:opacity-75">
+              {link.text}
+            </a>
           </Link>
         ))}
       </div>
