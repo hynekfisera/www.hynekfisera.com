c3b1ef72e19bad91217ae2c96e95e94f7f90e942
Hynek Fišera <hynek.fisera@seznam.cz>
2022-11-28 09:24:44 +0100

    BMC link

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,12 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <footer className="max-w-screen-xl mx-auto p-8 pt-12 flex flex-col gap-2">
-      <div className="text-center text-gray-700 font-medium">&copy; Hynek Fišera 2017-{new Date().getFullYear()}</div>
+      <div className="text-center text-gray-600 font-medium">
+        &copy; Hynek Fišera 2017-{new Date().getFullYear()} |{" "}
+        <Link href="https://www.buymeacoffee.com/hynekfisera" className="text-yellow-500 font-normal hover:underline">
+          Buy Me a Coffee
+        </Link>
+      </div>
       <div className="text-center text-gray-500">
         Built {"&"} deployed using{" "}
         <Link href="https://nextjs.org/" className="text-indigo-400 hover:underline">
