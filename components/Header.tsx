c6f23a44b9d5ce0abe1219aea45405eebf63c996
Hynek Fišera <hynek.fisera@seznam.cz>
2022-12-01 09:19:32 +0100

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -27,20 +27,24 @@ export default function Header() {
       <Link href="/" onClick={() => setVisible(false)} className="select-none text-lg font-semibold uppercase tracking-wider text-transparent bg-clip-text bg-gradient-to-br from-indigo-600 to-purple-600">
         Hynek Fišera
       </Link>
-      <FontAwesomeIcon icon={faBars} className={`text-xl p-1 text-slate-700 sm:!hidden cursor-pointer`} onClick={() => setVisible((v) => !v)} />
-      <div className={`${visible ? "flex" : "hidden"} sm:items-center absolute sm:static bg-gray-50 sm:bg-transparent top-0 left-0 right-0 mt-20 sm:mt-0 pl-8 pb-8 sm:p-0 border-b sm:border-none sm:!flex flex-col sm:flex-row gap-2 sm:gap-6`}>
-        {links.map((link) => (
-          <Link href={link.href} key={link.href} onClick={() => setVisible(false)} className="select-none uppercase tracking-wider hover:underline decoration-gray-500">
-            {link.text}
+      <div className="flex gap-4">
+        <div className="flex sm:flex-row-reverse gap-2 sm:gap-6">
+          <Link href={asPath} onClick={() => setVisible(false)} locale={locale === "en" ? "cs" : "en"} className="text-xs font-semibold group text-slate-600 select-none uppercase tracking-wider py-1.5">
+            <span className="text-slate-400 group-hover:text-slate-600">{locale === "en" ? "cs" : "en"} | </span>
+            {locale === "en" ? "en" : "cs"}
           </Link>
-        ))}
-        <a href="mailto:hynek@flairleap.com" onClick={() => setVisible(false)} className="select-none uppercase tracking-wider hover:underline decoration-gray-500">
-          {t("contact")}
-        </a>
-        <Link href={asPath} onClick={() => setVisible(false)} locale={locale === "en" ? "cs" : "en"} className="text-xs font-semibold group text-slate-600 select-none uppercase tracking-wider py-1.5">
-          <span className="text-slate-400 group-hover:text-slate-600">{locale === "en" ? "cs" : "en"} | </span>
-          {locale === "en" ? "en" : "cs"}
-        </Link>
+          <div className={`${visible ? "flex" : "hidden"} sm:items-center absolute sm:static bg-gray-50 sm:bg-transparent top-0 left-0 right-0 mt-20 sm:mt-0 pl-8 pb-8 sm:p-0 border-b sm:border-none sm:!flex flex-col sm:flex-row gap-2 sm:gap-6`}>
+            {links.map((link) => (
+              <Link href={link.href} key={link.href} onClick={() => setVisible(false)} className="select-none uppercase tracking-wider hover:underline decoration-gray-500">
+                {link.text}
+              </Link>
+            ))}
+            <a href="mailto:hynek@flairleap.com" onClick={() => setVisible(false)} className="select-none uppercase tracking-wider hover:underline decoration-gray-500">
+              {t("contact")}
+            </a>
+          </div>
+        </div>
+        <FontAwesomeIcon icon={faBars} className={`text-xl p-1 text-slate-700 sm:!hidden cursor-pointer`} onClick={() => setVisible((v) => !v)} />
       </div>
     </header>
   );
