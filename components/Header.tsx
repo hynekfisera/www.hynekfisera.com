d07f490fa3a2179a68e7b22fe77daa43cd23edf4
Hynek Fišera <hynek.fisera@seznam.cz>
2023-09-12 17:36:32 +0200

    Add navbar CTA

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,15 +1,22 @@
 import React, { useState } from "react";
 import Link from "next/link";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { IconDefinition, faBars, faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { useTranslation } from "next-i18next";
 import { useRouter } from "next/router";
+
+type Link = {
+  text: string;
+  href: string;
+  button?: boolean;
+  icon?: IconDefinition;
+};
 
 export default function Header() {
   const { t } = useTranslation("header");
   const { locale, asPath } = useRouter();
 
-  const links = [
+  const links: Array<Link> = [
     {
       text: "Portfolio",
       href: "/",
@@ -19,8 +26,14 @@ export default function Header() {
       href: "/about-me",
     },
     {
-      text: t("social"),
+      text: t("links"),
       href: "/links",
+    },
+    {
+      text: t("contact"),
+      href: "mailto:hynek@flairleap.com",
+      button: true,
+      icon: faEnvelope,
     },
   ];
 
@@ -32,17 +45,35 @@ export default function Header() {
         Hynek Fišera
       </Link>
       <div className="flex gap-4">
-        <div className="flex sm:flex-row-reverse gap-2 sm:gap-6">
-          <Link href={asPath} onClick={() => setVisible(false)} locale={locale === "en" ? "cs" : "en"} title={locale === "en" ? "Switch language" : "Změnit jazyk"} className="text-xs font-semibold group text-slate-600 select-none uppercase tracking-wider py-1.5">
-            <span className="text-slate-400 group-hover:text-slate-600">{locale === "en" ? "cs" : "en"} | </span>
-            {locale === "en" ? "en" : "cs"}
-          </Link>
-          <div className={`${visible ? "flex" : "hidden"} sm:items-center absolute sm:static bg-white sm:bg-transparent top-0 left-0 right-0 mt-20 sm:mt-0 pl-8 pb-8 sm:p-0 border-b sm:border-none sm:!flex flex-col sm:flex-row gap-2 sm:gap-6`}>
-            {links.map((link) => (
-              <Link href={link.href} key={link.href} onClick={() => setVisible(false)} className="select-none uppercase tracking-wider hover:underline decoration-gray-500">
-                {link.text}
-              </Link>
-            ))}
+        <div className="flex sm:flex-row-reverse gap-2 sm:gap-4">
+          <div className="grid place-items-center">
+            <Link
+              href={asPath}
+              onClick={() => setVisible(false)}
+              locale={locale === "en" ? "cs" : "en"}
+              title={locale === "en" ? "Switch language" : "Změnit jazyk"}
+              className="text-[0.6rem] bg-slate-100 border border-slate-200 rounded-xl font-semibold group text-slate-600 select-none uppercase tracking-wider px-2.5 py-1"
+            >
+              <span className="text-slate-400 group-hover:text-slate-600">{locale === "en" ? "cs" : "en"} | </span>
+              {locale === "en" ? "en" : "cs"}
+            </Link>
+          </div>
+          <div className={`${visible ? "flex" : "hidden"} items-end sm:items-center absolute sm:static bg-white sm:bg-transparent top-0 left-0 right-0 mt-20 sm:mt-0 px-10 pb-8 sm:p-0 border-b sm:border-none sm:!flex flex-col sm:flex-row gap-3 sm:gap-6`}>
+            {links.map((link) => {
+              if (link.button) {
+                return (
+                  <Link href={link.href} key={link.href} onClick={() => setVisible(false)} className="btn btn-primary">
+                    {link.text} {link.icon && <FontAwesomeIcon icon={link.icon} className="text-sm" />}
+                  </Link>
+                );
+              } else {
+                return (
+                  <Link href={link.href} key={link.href} onClick={() => setVisible(false)} className="select-none font-medium tracking-wide text-slate-600 hover:underline decoration-gray-500">
+                    {link.text}
+                  </Link>
+                );
+              }
+            })}
           </div>
         </div>
         <FontAwesomeIcon icon={faBars} className={`text-xl p-1 text-slate-700 sm:!hidden cursor-pointer`} onClick={() => setVisible((v) => !v)} />
